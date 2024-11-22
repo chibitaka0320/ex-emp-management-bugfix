376c4c6e492872040a83204d4aac45e36a1202fe
takahiro <taka.araat32@gmail.com>
2024-11-22 15:19:09 +0900

    管理者登録：確認用パスワードの追加

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -76,6 +76,10 @@ public class AdministratorController {
 	 */
 	@PostMapping("/insert")
 	public String insert(@Validated InsertAdministratorForm form, BindingResult result) {
+		if (!form.getPassword().equals(form.getConfirmPassword())) {
+			result.rejectValue("confirmPassword", null, "パスワードが一致しません");
+		}
+
 		if (result.hasErrors()) {
 			return toInsert();
 		}
